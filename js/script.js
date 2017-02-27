bf0ff433c514958054635590ccd3c710b665eb5a
Nick Chin <shardsif@gmail.com>
2017-02-27 19:10:27 +1100

    Nav transition now fixed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 $( document ).ready(function() {
 
   $(window).on("scroll", function() {
-      /* Removed for now
+      /* Removed for now */
       if($(window).scrollTop() > 50) {
           $("header").addClass("active-nav");
       } else {
           //remove the background property so it comes transparent again (defined in your css)
          $("header").removeClass("active-nav");
       }
-      */
   });
   $(".dropdown-nav").click(function(){
     $(this).children(".dropdown-content").slideToggle();
