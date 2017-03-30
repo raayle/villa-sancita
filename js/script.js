01e02896ced0eb6fb2fb4aa905ffc629149615f7
Nick Chin <shardsif@gmail.com>
2017-03-30 16:50:26 +1100

    Added contact us page

--- js/script.js
+++ js/script.js
@@ -61,4 +61,40 @@ $( document ).ready(function() {
   $grid.imagesLoaded().progress( function() {
     $grid.masonry('layout');
   });
+
+  // *** Contact us form ***
+  var $contactForm = $('#contact-form');
+  $contactForm.submit(function(e) {
+	e.preventDefault();
+	var $submit = $('input:submit', $contactForm);
+	var defaultSubmitText = $submit.val();
+
+	$.ajax({
+		url: '//formspree.io/nick.x.chin@gmail.com',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: 'json',
+		beforeSend: function() {
+			//$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+			$submit.attr('disabled', true).val('Sending message…');
+		},
+		success: function(data) {
+			//$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+			$submit.val('Message sent!').addClass('form-success');
+			setTimeout(function() {
+				//$('.alert--success').remove();
+				$submit.attr('disabled', false).val(defaultSubmitText).removeClass('form-success');
+			}, 10000);
+		},
+		error: function(err) {
+			//$contactForm.find('.alert--loading').hide();
+			//$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+			$submit.val('There was an error sending the message. Please try again.');
+			setTimeout(function() {
+				//$('.alert--error').remove();
+				$submit.attr('disabled', false).val(defaultSubmitText);
+			}, 8000);
+		}
+	});
+});
 });
